4b241086114ef52ed39f8719e8c0fb1b3a447a34
fuweizhang <v-fuweizhang@CREDITEASE.CN>
2022-07-12 18:25:18 +0800

    代码修改

--- test_run/run.py
+++ test_run/run.py
@@ -7,5 +7,5 @@ from time import sleep
 from lib.get_delete_report import *
 from creat_report.creat_report import *
 if __name__ == '__main__':
-    test_remove()   #删除报告
-    # report().test_creat_BFreport()  #执行用例生成报告
+    # test_remove()   #删除报告
+    report().test_creat_BFreport()  #执行用例生成报告
